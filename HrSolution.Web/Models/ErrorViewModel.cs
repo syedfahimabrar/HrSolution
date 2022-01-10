5726219b5c708330426c55c282c96db0e3c2d66c
Fahim Abrar <fahimabrar13@gmail.com>
2022-01-10 21:15:46 +0600

    remove unwanted code

--- HrSolution.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HrSolution.Web.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
